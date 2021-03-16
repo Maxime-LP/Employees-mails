54df5247f89db4f5e17a58f6c7b473c9e2de6895
Maxime-LP <71880772+Maxime-LP@users.noreply.github.com>
2021-03-16 18:11:15 +0100

    populate update

--- mailsproject/mailsapp1/models.py
+++ mailsproject/mailsapp1/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 
-class employee(models.Model):
+class mailbox(models.Model):
+    tag = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     first_name = models.CharField(max_length=40)
     category = models.CharField(max_length=40,default='Employee')
 
-class mailbox(models.Model):
-    tag = models.CharField(max_length=60)
-    employee = models.ForeignKey(employee, on_delete=models.CASCADE)
-    mail1 = models.CharField(max_length=60,null=True)
-    mail2 = models.CharField(max_length=60,null=True)
-    mail3 = models.CharField(max_length=60,null=True)
+class mail_address(models.Model):
+    box = models.ForeignKey(mailbox, on_delete=models.CASCADE)
+    address = models.EmailField(max_length=80)
 
 class mail(models.Model):
-    mail_date = models.DateField(null=True)
-    recipient_mailbox = models.ForeignKey(mailbox, on_delete=models.CASCADE,related_name='recipient_mailbox_id')
-    sender_mailbox = models.ForeignKey(mailbox, on_delete=models.CASCADE,related_name='sender_mailbox_id')
+    mail_date = models.DateTimeField(null=False)
+    recipient_mail = models.ForeignKey(mail_address, on_delete=models.CASCADE,related_name='recipient_mail_id')
+    #il peut y avoir plusieurs destinataires
+    sender_mail = models.ForeignKey(mail_address, on_delete=models.CASCADE,related_name='sender_mail_id')
     previous_mail = models.ForeignKey("self",null=True,on_delete=models.SET_NULL,related_name='previous_mail_id')
     response_mail = models.ForeignKey("self",null=True,on_delete=models.SET_NULL,related_name='response_mail_id')
+    #il peut y avoir plusieurs resp
--- mailsproject/populate.py
+++ mailsproject/populate.py
@@ -3,47 +3,44 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mailsproject.settings')
 import django
 django.setup()
 
-from mailsapp1.models import employee,mail,mailbox
+from mailsapp1.models import mailbox,mail_address,mail
 import xml.etree.ElementTree as ET
 import zipfile
 
 path = r'C:\Users\lepau\OneDrive\Desktop'
 
-#csv file
-tree = ET.parse(path+'\employes_enron.xml')
+#xml file
+tree = ET.parse(path + '\employes_enron.xml')
 root = tree.getroot()
 
-###Populating employee and mailbox databases
+###Populating mailbox and mail_address databases
+#already populated
 """
 for child in root:
-    current_employee = employee()
     current_mailbox = mailbox()
-    
+    current_mailbox.save()
+
     try:
-        current_employee.category = child.attrib['category']
+        current_mailbox.category = child.attrib['category']
     except KeyError:
-        current_employee.category = 'Employee'
+        current_mailbox.category = 'Employee'
 
     for subchild in child:
         if subchild.tag == 'lastname':
-            current_employee.last_name = subchild.text
+            current_mailbox.last_name = subchild.text
 
         elif subchild.tag == 'firstname':
-            current_employee.first_name = subchild.text
+            current_mailbox.first_name = subchild.text
 
         elif subchild.tag == 'email':
-            if current_mailbox.mail1 == None:
-                current_mailbox.mail1 = subchild.attrib['address']
-            elif current_mailbox.mail2 == None:
-                current_mailbox.mail2 = subchild.attrib['address']
-            elif current_mailbox.mail3 == None:
-                current_mailbox.mail3 = subchild.attrib['address']
-
-        elif subchild.tag =='mailbox':
+            new_mail = mail_address()
+            new_mail.box_id = current_mailbox.id
+            new_mail.address = subchild.attrib['address']
+            new_mail.save()
+        
+        elif subchild.tag == 'mailbox':
             current_mailbox.tag = subchild.text
-    
-    current_employee.save()
-    current_mailbox.employee_id = current_employee.id
+
     current_mailbox.save()
 """
 
