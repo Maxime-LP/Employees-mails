32ab2a15b8ef15dc81c9f0efbb8c987b8661feb8
Maxime-LP <71880772+Maxime-LP@users.noreply.github.com>
2021-04-23 16:54:49 +0200

    Update models.py

--- emd-project/app/models.py
+++ emd-project/app/models.py
@@ -38,7 +38,7 @@ class user(models.Model):
     category = models.CharField(null=True,max_length=40)
 
     def __str__(self):
-        return self.name, self.inEnron, self.category
+        return f"{self.name}, {self.inEnron}, {self.category}"
 
 
 
