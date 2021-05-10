0a3dfc5b24b4aec95b775b81830703096416fd7a
Maxime-LP <71880772+Maxime-LP@users.noreply.github.com>
2021-05-10 14:15:05 +0200

    sidenav update

--- emd-project/app/views.py
+++ emd-project/app/views.py
@@ -160,11 +160,6 @@ def days(request):
     else:
         threshold = int(threshold)
     
-    lines = request.GET.get('lines')
-    if not lines:
-        lines = 10
-    else:
-        lines = int(lines)
 
     mails_per_day = Mail.objects.annotate(time=TruncDate('date')).values('time')\
                     .filter(date__gte=start_date,date__lte=end_date).annotate(dcount=Count('enron_id')).order_by('-dcount').filter(dcount__gte=threshold)
