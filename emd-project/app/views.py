ec968992b1c4fe27428d1c890d6578f778d65a9e
Maxime-LP <71880772+Maxime-LP@users.noreply.github.com>
2021-05-14 22:52:32 +0200

    minor changes

--- emd-project/app/views.py
+++ emd-project/app/views.py
@@ -98,10 +98,10 @@ def employees(request):
     try:
         mci = paginator.page(page)
     except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
+        # If page is not an integer, delivers first page.
         mci = paginator.page(1)
     except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
+        # If page is out of range (e.g. 9999), delivers last page of results.
         mci = paginator.page(paginator.num_pages)
 
     context = {
